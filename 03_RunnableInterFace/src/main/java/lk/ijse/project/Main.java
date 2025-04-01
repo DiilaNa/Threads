8f0a9c1111b0f8019eef6df65fd4907b78a1fec5
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-01 11:04:49 +0530

    03-Runnable InterFace add

---
+++ 03_RunnableInterFace/src/main/java/lk/ijse/project/Main.java
@@ -0,0 +1,45 @@
+package lk.ijse.project;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.run();
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        B b = new B();
+        b.run();
+    }
+}
+    class A implements Runnable {
+
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("A");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    class B implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("B");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+    }
